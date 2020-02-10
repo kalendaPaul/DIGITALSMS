966cf8b814a8ab89f73c9554168444f589d095c6
unknown <kpaul7747@gmail.com>
2020-02-10 09:05:14 +0000

    initial commit

---
+++ src/main/java/com/dewcis/sms/Device.java
@@ -0,0 +1,303 @@
+package com.dewcis.sms;
+
+import java.util.Date;
+import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Device {
+	Logger log = Logger.getLogger(Device.class.getName());
+	String baseUrl= "https://dtsvc.safaricom.com:8480/api";
+
+	//Getting sessionid for the logged in user
+	public static void main(String[] args) {
+		String url = "https://dtsvc.safaricom.com:8480/api";
+		String username = "Etiqet_apiuser";
+		String password = "Admin@123";
+
+		JSONObject jLogin = new JSONObject();
+		jLogin.put("username", username);
+		jLogin.put("password", password);
+
+		httpClient client = new httpClient();
+		String userFile = "/auth/login";
+       	String uri = url + userFile;
+		String result = client.getTokens(uri, jLogin.toString());
+		JSONObject json = new JSONObject(result);
+		String token = json.getString("token");
+
+		Device log = new Device();
+		String refreshToken = log.rfToken(json.getString("refreshToken"));
+		if(refreshToken.length()!=0){
+			System.out.println("Tokens have expired" + refreshToken.length());
+			// Transform reponse to JSon Object
+			json = new JSONObject(result);
+			token = json.getString("token");
+			log.bulkSms(token);
+			log.sendSms(token);
+			log.subscr(token);
+			log.unsubscr(token);
+			log.subscr(token);
+		}else if (refreshToken.length()==0) {
+			System.out.println("Tokens have not yet expired " + refreshToken.length());
+			log.bulkSms(token);
+			log.sendSms(token);
+			log.subscr(token);
+			log.unsubscr(token);
+			log.subscr(token);
+		}
+
+	}
+
+	//refreshing user token in case it expires or before it expires
+	public String rfToken(String refreshToken) {
+	    String userFile = "/auth/RefreshToken";
+	    String uri = baseUrl + userFile;
+	    httpClient get = new httpClient();
+	    String results = get.getRefreshTokens(uri, refreshToken);
+
+	    return results;
+
+   }
+
+   // user subscription 
+	public String subscr(String token) {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); //get current timestamp and its format
+		System.out.println("Response timestamp : " + timeStamp);
+
+		JSONObject jdatav1 = new JSONObject();
+		jdatav1.put("name", "OfferCode");
+		jdatav1.put("value", "350032100559");
+
+		JSONObject jdatav2 = new JSONObject();
+		jdatav2.put("name", "Msisdn");
+		jdatav2.put("value", "795421629");
+
+		JSONObject jdatav3 = new JSONObject();
+		jdatav3.put("name", "Language");
+		jdatav3.put("value", "1");
+
+		JSONObject jdatav4 = new JSONObject();
+		jdatav4.put("name", "CpId");
+		jdatav4.put("value", "104");
+
+
+		JSONArray jdataset = new JSONArray();
+		jdataset.put(jdatav1);
+		jdataset.put(jdatav2);
+		jdataset.put(jdatav3);
+		jdataset.put(jdatav4);
+
+		
+		JSONObject jdata = new JSONObject();
+		jdata.put("data", jdataset);
+
+		JSONObject jsubscr = new JSONObject();
+		jsubscr.put("requestId", "17");
+		jsubscr.put("requestTimeStamp", timeStamp);
+		jsubscr.put("channel", "SMS");
+		jsubscr.put("operation", "ACTIVATE");
+		jsubscr.put("requestParam", jdata);
+
+		System.out.println("Response packet json values : " + jsubscr.toString());
+
+	    String userFile = "/public/SDP/activate";
+	    String uri = baseUrl + userFile;
+	    httpClient get = new httpClient();
+	    String results = get.post(uri, token, jsubscr.toString());
+
+	    return results;
+
+   }
+
+   // user De-activation 
+	public String unsubscr(String token) {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); //get current timestamp and its format
+		System.out.println("Response timestamp : " + timeStamp);
+
+		JSONObject jdatav1 = new JSONObject();
+		jdatav1.put("name", "OfferCode");
+		jdatav1.put("value", "1001");
+
+		JSONObject jdatav2 = new JSONObject();
+		jdatav2.put("name", "Msisdn");
+		jdatav2.put("value", "254720123456");
+
+		JSONObject jdatav3 = new JSONObject();
+		jdatav3.put("name", "CpId");
+		jdatav3.put("value", "104");
+
+
+		JSONArray jdataset = new JSONArray();
+		jdataset.put(jdatav1);
+		jdataset.put(jdatav2);
+		jdataset.put(jdatav3);
+
+		
+		JSONObject jdata = new JSONObject();
+		jdata.put("data", jdataset);
+
+		JSONObject junsubscr = new JSONObject();
+		junsubscr.put("requestId", "17");
+		junsubscr.put("requestTimeStamp", timeStamp);
+		junsubscr.put("channel", "3");
+		junsubscr.put("sourceAddress", "224.223.10.27");
+		junsubscr.put("operation", "DEACTIVATE");
+		junsubscr.put("requestParam", jdata);
+
+		System.out.println("Response packet json values : " + junsubscr.toString());
+
+	    String userFile = "/public/SDP/deactivate";
+	    String uri = baseUrl + userFile;
+	    httpClient get = new httpClient();
+	    String results = get.post(uri, token, junsubscr.toString());
+
+	    return results;
+
+   }
+
+	// user bulk sms
+	public String bulkSms(String token) {
+
+		JSONArray jdataset = new JSONArray();
+		JSONObject jdata = new JSONObject();
+		jdata.put("userName", "Etiqet_apiuser");
+		jdata.put("channel", "sms");
+		jdata.put("packageId", "4391");
+		jdata.put("oa", "TestSender");
+		jdata.put("msisdn", "254758934575,254795421629");
+		jdata.put("message", "hello testing online promo nov 1");
+		jdata.put("uniqueId", "2500688298721");
+		jdata.put("actionResponseURL", "http://72.1.51.93:8485/BulkDLR");
+
+		jdataset.put(jdata);
+
+		JSONObject jbulksms = new JSONObject();
+		jbulksms.put("timeStamp", "15567981561235");
+		jbulksms.put("dataSet", jdataset);
+
+
+	    String userFile = "/public/CMS/bulksms";
+	    String uri = baseUrl + userFile;
+	    httpClient get = new httpClient();
+	    String results = get.post(uri, token, jbulksms.toString());
+
+	    return results;
+
+	}
+
+   // user one sms
+	public String sendSms(String token) {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); //get current timestamp and its format
+		System.out.println("Response timestamp : " + timeStamp);
+
+		JSONObject jdatav1 = new JSONObject();
+		jdatav1.put("name", "LinkId");
+		jdatav1.put("value", "00010310189519161781865526");
+
+		JSONObject jdatav2 = new JSONObject();
+		jdatav2.put("name", "Msisdn");
+		jdatav2.put("value", "254795421629");
+
+		JSONObject jdatav3 = new JSONObject();
+		jdatav3.put("name", "Content");
+		jdatav3.put("value", "Thank You for Ondemand Subscription SAFRI TEST TUN Subscption test Send sms");
+
+		JSONObject jdatav4 = new JSONObject();
+		jdatav4.put("name", "OfferCode");
+		jdatav4.put("value", "1003");
+
+		JSONObject jdatav5 = new JSONObject();
+		jdatav5.put("name", "CpId");
+		jdatav5.put("value", "104");
+
+		JSONArray jdataset = new JSONArray();
+		jdataset.put(jdatav1);
+		jdataset.put(jdatav2);
+		jdataset.put(jdatav3);
+		jdataset.put(jdatav4);
+		jdataset.put(jdatav5);
+
+		
+		JSONObject jdata = new JSONObject();
+		jdata.put("data", jdataset);
+
+		JSONObject jsendsms = new JSONObject();
+		jsendsms.put("requestId", "17");
+		jsendsms.put("requestTimeStamp", timeStamp);
+		jsendsms.put("channel", "3");
+		jsendsms.put("sourceAddress", "224.223.10.27");
+		jsendsms.put("operation", "SendSMS");
+		jsendsms.put("requestParam", jdata);
+
+		System.out.println("Response packet json values : " + jsendsms.toString());
+
+
+	    String userFile = "public/SDP/sendSMSRequest";
+	    String uri = baseUrl + userFile;
+	    httpClient get = new httpClient();
+	    String results = get.post(uri, token, jsendsms.toString());
+
+	    return results;
+
+   }
+
+   // CP Notification 
+	public void notification(String token) {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); //get current timestamp and its format
+		System.out.println("Response timestamp : " + timeStamp);
+
+		JSONObject jdatav1 = new JSONObject();
+		jdatav1.put("name", "OfferCode");
+		jdatav1.put("value", "1001");
+
+		JSONObject jdatav2 = new JSONObject();
+		jdatav2.put("name", "Msisdn");
+		jdatav2.put("value", "254720123456");
+
+		JSONObject jdatav3 = new JSONObject();
+		jdatav3.put("name", "Command");
+		jdatav3.put("value", "XXX" ); //Refer Input parameters
+
+
+		JSONArray jdataset = new JSONArray();
+		jdataset.put(jdatav1);
+		jdataset.put(jdatav2);
+		jdataset.put(jdatav3);
+
+		JSONObject jadditionalData = new JSONObject();
+		jadditionalData.put("name", "");
+		jadditionalData.put("value", "");
+
+		JSONArray jadditionalDataSet = new JSONArray();
+		jadditionalDataSet.put(jadditionalData);
+		
+		JSONObject jdata = new JSONObject();
+		jdata.put("data", jdataset);
+
+		JSONObject jsendsms = new JSONObject();
+		jsendsms.put("requestId", "17223344555");
+		jsendsms.put("requestTimeStamp", timeStamp);
+		jsendsms.put("operation", "CP_NOTIFICATION");
+		jsendsms.put("requestParam", jdata);
+		jsendsms.put("additionalData", jadditionalDataSet);
+
+		System.out.println("Response packet json values : " + jsendsms.toString());
+
+	    // String userFile = "/public/SDP/deactivate";
+	    // String uri = baseUrl + userFile;
+	    // httpClient get = new httpClient();
+	    // String results = get.post(uri, token);
+
+	    // return results;
+
+   }
+
+
+}
---
+++ src/main/java/com/dewcis/sms/httpClient.java
@@ -0,0 +1,209 @@
+package com.dewcis.sms;
+
+import java.util.logging.Logger;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.RequestBody;
+import okhttp3.MediaType;
+import okhttp3.Call;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.SSLSocketFactory;
+
+import java.security.cert.CertificateException;
+
+import org.json.JSONObject;
+
+public class httpClient {
+	Logger log = Logger.getLogger(httpClient.class.getName());
+
+	// String result = null;
+	// String token = null;
+	// String refreshToken = null;
+	OkHttpClient client = null;
+	
+
+private static OkHttpClient getUnsafeOkHttpClient() {
+	try {
+		// Create a trust manager that does not validate certificate chains
+		final TrustManager[] trustAllCerts = new TrustManager[] {
+		new X509TrustManager() {
+		@Override
+		public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+		}
+
+		@Override
+		public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+		}
+
+		@Override
+		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+		return new java.security.cert.X509Certificate[]{};
+		}
+		}
+		};
+
+		// Install the all-trusting trust manager
+		final SSLContext sslContext = SSLContext.getInstance("SSL");
+		sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+		// Create an ssl socket factory with our all-trusting manager
+		final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+		OkHttpClient.Builder builder = new OkHttpClient.Builder();
+		builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+		builder.hostnameVerifier(new HostnameVerifier() {
+		@Override
+		public boolean verify(String hostname, SSLSession session) {
+		return true;
+		}
+		});
+
+		OkHttpClient okHttpClient = builder.build();
+		return okHttpClient;
+		} catch (Exception e) {
+		throw new RuntimeException(e);
+	}
+}
+	
+public String getTokens(String bURL, String sData) {
+   // String json = "{\"id\":1,\"name\":\"John\"}";
+		client = getUnsafeOkHttpClient();
+		String result = null;
+
+	try {
+
+
+	    RequestBody body = RequestBody.create(
+	    MediaType.parse("application/json"), sData);
+
+	    Request request = new Request.Builder()
+		  .header("X-Requested-With", "XMLHttpRequest")
+	      .url(bURL)
+	      .post(body)
+	      .build();
+
+	    Call call = client.newCall(request);
+	    Response response = call.execute();
+	    result = response.body().string();     // Get response
+	    int responseCode = 0;
+
+		if ((responseCode = response.code()) == 200) {
+
+		    System.out.println("Response body : " + result);
+	    	// Transform reponse to JSon Object
+           JSONObject json = new JSONObject(result);
+           JOptionPane.showMessageDialog(null, json.getString("msg"));
+           // Use the JSon Object to get token value
+           // token = json.getString("token");
+           // refreshToken = json.getString("refreshToken");
+
+		}else if((responseCode = response.code()) != 200) {
+			JSONObject jObject = new JSONObject(result);
+			JOptionPane.showMessageDialog(null, jObject.getString("msg"));
+			System.out.println("IO Error : " + jObject);
+		}
+
+
+   } catch (IOException ex) {
+		System.out.println("IO Error : " + ex);
+	}
+
+   return result;
+}
+
+public String getRefreshTokens(String bURL, String refreshToken) {
+	client = getUnsafeOkHttpClient();
+	String result = null;
+	try {
+
+	    Request request = new Request.Builder()
+                .url(bURL)
+                .header("X-Requested-With", "XMLHttpRequest")
+                .header("X-Authorization", "Bearer"+refreshToken+"" )
+                .header("Content-Type", "application/json")
+                .build();
+
+	    Call call = client.newCall(request);
+	    Response response = call.execute();
+	    result = response.body().string();     // Get response
+	    int responseCode = 0;
+
+		if ((responseCode = response.code()) == 200) {
+
+		    System.out.println("Response body Token refresh : " + result);
+		    if(result.length()!=0){
+				System.out.println("Tokens have expired");
+		    	// Transform reponse to JSon Object
+	           JSONObject json = new JSONObject(result);
+				result = json.getString("token");
+		    }
+	    	
+		}else if((responseCode = response.code()) != 200) {
+			JSONObject jObject = new JSONObject(result);
+			JOptionPane.showMessageDialog(null, jObject.getString("msg"));
+			System.out.println("IO Error : " + jObject);
+		}
+
+
+   } catch (IOException ex) {
+		System.out.println("IO Error : " + ex);
+	}
+
+   return result;
+}
+
+public String post(String bURL, String token, String jbody) {
+	client = getUnsafeOkHttpClient();
+	String result = null;
+	try {
+
+		RequestBody body = RequestBody.create(
+	    MediaType.parse("application/json"), jbody);
+
+	    Request request = new Request.Builder()
+                .url(bURL)
+                .header("X-Authorization", "Bearer "+token+"" )
+                // .header("Content-Type", "application/json")
+                .header("X-Requested-With", "XMLHttpRequest")
+                .post(body)
+                .build();
+
+	    Call call = client.newCall(request);
+	    Response response = call.execute();
+	    result = response.body().string();     // Get response
+	    int responseCode = 0;
+
+		if ((responseCode = response.code()) == 200) {
+
+		    System.out.println("Response body bulksms : " + result);
+//		    if(result.isEmpty()){
+//		    	// Transform reponse to JSon Object
+//	           JSONObject json = new JSONObject(result);
+//	           token = json.getString("token");
+//		    }
+	    	
+		}else if((responseCode = response.code()) != 200) {
+			// JSONObject jObject = new JSONObject(result);
+			// JOptionPane.showMessageDialog(null, jObject.getString("msg"));
+			System.out.println("IO Error Response body bulksms : " + result);
+		}
+
+
+   } catch (IOException ex) {
+		System.out.println("IO Error : " + ex);
+	}
+
+   return result;
+}
+	
+}
+
